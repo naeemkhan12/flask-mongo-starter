083cc9e960a4cc42294ae21931d3ca2ffdc4d7ae
naeemkhan12 <naeemb7070@gmail.com>
2018-04-21 10:36:29 +0500

    config for charts

--- app/app.py
+++ app/app.py
@@ -6,9 +6,6 @@ from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
-
-app.config['MONGO_HOST'] = 'localhost'
-app.config['MONGO_PORT']='27017'
 app.config['MONGO_DBNAME']="lsdda"
 
 # connect to MongoDB with the defaults
